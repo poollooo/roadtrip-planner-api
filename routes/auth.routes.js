fcce20b773a6889806571c837e7668384ae64f8a
Paul Nizet <polonizet@gmail.com>
2022-08-25 10:26:18 +0200

    fix eslint

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,36 +1,37 @@
-const router = require("express").Router();
+/* eslint-disable max-len */
+const router = require('express').Router();
 
 // ℹ️ Handles password encryption
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
 // How many rounds should bcrypt run the salt (default [10 - 12 rounds])
 const saltRounds = 10;
 
 // Require the User model in order to interact with the database
-const User = require("../models/User.model");
+const User = require('../models/User.model');
 
 // Require necessary (isLoggedOut and isLoggedIn) middleware in order to control access to specific routes
-const isLoggedOut = require("../middleware/isLoggedOut");
-const isLoggedIn = require("../middleware/isLoggedIn");
+const isLoggedOut = require('../middleware/isLoggedOut');
+const isLoggedIn = require('../middleware/isLoggedIn');
 
-router.get("/loggedin", (req, res) => {
+router.get('/loggedin', (req, res) => {
   res.json(req.user);
 });
 
-router.post("/signup", isLoggedOut, (req, res) => {
+router.post('/signup', isLoggedOut, (req, res) => {
   const { username, password } = req.body;
 
   if (!username) {
     return res
       .status(400)
-      .json({ errorMessage: "Please provide your username." });
+      .json({ errorMessage: 'Please provide your username.' });
   }
 
   if (password.length < 8) {
     return res
       .status(400)
-      .json({ errorMessage: "Your password needs to be at least 8 characters long." });
+      .json({ errorMessage: 'Your password needs to be at least 8 characters long.' });
   }
 
   //   ! This use case is using a regular expression to control for special characters and min length
@@ -51,20 +52,19 @@ router.post("/signup", isLoggedOut, (req, res) => {
     if (found) {
       return res
         .status(400)
-        .json({ errorMessage: "Username already taken." });
+        .json({ errorMessage: 'Username already taken.' });
     }
 
     // if user is not found, create a new user - start with hashing the password
     return bcrypt
       .genSalt(saltRounds)
       .then((salt) => bcrypt.hash(password, salt))
-      .then((hashedPassword) => {
+      .then((hashedPassword) =>
         // Create a user and save it in the database
-        return User.create({
+        User.create({
           username,
           password: hashedPassword,
-        });
-      })
+        }))
       .then((user) => {
         // Bind the user to the session object
         req.session.user = user;
@@ -77,7 +77,7 @@ router.post("/signup", isLoggedOut, (req, res) => {
         if (error.code === 11000) {
           return res.status(400).json({
             errorMessage:
-              "Username need to be unique. The username you chose is already in use.",
+              'Username need to be unique. The username you chose is already in use.',
           });
         }
         return res.status(500).json({ errorMessage: error.message });
@@ -85,20 +85,20 @@ router.post("/signup", isLoggedOut, (req, res) => {
   });
 });
 
-router.post("/login", isLoggedOut, (req, res, next) => {
+router.post('/login', isLoggedOut, (req, res, next) => {
   const { username, password } = req.body;
 
   if (!username) {
     return res
       .status(400)
-      .json({ errorMessage: "Please provide your username." });
+      .json({ errorMessage: 'Please provide your username.' });
   }
 
   // Here we use the same logic as above
   // - either length based parameters or we check the strength of a password
   if (password.length < 8) {
     return res.status(400).json({
-      errorMessage: "Your password needs to be at least 8 characters long.",
+      errorMessage: 'Your password needs to be at least 8 characters long.',
     });
   }
 
@@ -107,13 +107,13 @@ router.post("/login", isLoggedOut, (req, res, next) => {
     .then((user) => {
       // If the user isn't found, send the message that user provided wrong credentials
       if (!user) {
-        return res.status(400).json({ errorMessage: "Wrong credentials." });
+        return res.status(400).json({ errorMessage: 'Wrong credentials.' });
       }
 
       // If user is found based on the username, check if the in putted password matches the one saved in the database
       bcrypt.compare(password, user.password).then((isSamePassword) => {
         if (!isSamePassword) {
-          return res.status(400).json({ errorMessage: "Wrong credentials." });
+          return res.status(400).json({ errorMessage: 'Wrong credentials.' });
         }
         req.session.user = user;
         // req.session.user = user._id; // ! better and safer but in this case we saving the entire user object
@@ -129,12 +129,12 @@ router.post("/login", isLoggedOut, (req, res, next) => {
     });
 });
 
-router.get("/logout", isLoggedIn, (req, res) => {
+router.get('/logout', isLoggedIn, (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       return res.status(500).json({ errorMessage: err.message });
     }
-    res.json({ message: "Done" });
+    res.json({ message: 'Done' });
   });
 });
 
