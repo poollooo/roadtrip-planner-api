a1d7464beef8a364608ac02756adc4b058faff81
Sebastien Niang <sebastien.niang@hotmail.fr>
2022-08-26 16:35:21 +0200

    rename filed authen

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,15 +1,3 @@
-// // !TODO - still need to check if auth is done via sessions or not
-// module.exports = (req, res, next) => {
-//   // checks if the user is logged in when trying to access a specific page
-//   if (!req.session.user) {
-//     return res
-//       .status(403)
-//       .json({ errorMessage: "You must be logged in to see this page" });
-//   }
-//   req.user = req.session.user;
-//   next();
-// };
-
 const jsonWebToken = require("jsonwebtoken");
 const User = require("../models/User.model");
 
