8b252548bf386d88416a42fade9a980f031bc6ed
Paul Nizet <polonizet@gmail.com>
2022-08-26 11:58:01 +0200

    make get locationId by City search return the right locationId

--- routes/locationSearch.routes.js
+++ routes/locationSearch.routes.js
@@ -1,10 +1,18 @@
 const axios = require('axios');
+<<<<<<< Updated upstream
+=======
+const router = require('express').Router();
+>>>>>>> Stashed changes
 
 const options = {
   method: 'GET',
   url: 'https://travel-advisor.p.rapidapi.com/locations/search',
   params: {
+<<<<<<< Updated upstream
     query: 'paris',
+=======
+    query: '',
+>>>>>>> Stashed changes
     limit: '30',
     offset: '0',
     units: 'km',
@@ -19,6 +27,7 @@ const options = {
   },
 };
 
+<<<<<<< Updated upstream
 axios
   .request(options)
   .then(({ data }) => {
@@ -26,3 +35,28 @@ axios
   }).catch((error) => {
     console.log(error);
   });
+=======
+// the route is {baseUrl}/api/search/:citySearched
+
+  router.get('/:citySearched', async (req, res, next) => {
+    try {
+        const { citySearched } = req.params;
+        options.params.query = citySearched;
+        
+        axios.request(options)
+        .then(({ data }) => {
+            const locationSearchedId = data.data[0].result_object.location_id;
+            console.log('locationSearchedId should be', locationSearchedId);
+            return res.status(200).json(locationSearchedId);
+        }).catch((error) => {
+            console.log(error);
+        });
+    } catch (error) {
+      next(error);
+    }
+  });
+
+
+module.exports = router;
+
+>>>>>>> Stashed changes
