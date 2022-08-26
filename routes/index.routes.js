fcd01a3eea43fff333e07c1e048d74fae025aaca
Paul Nizet <polonizet@gmail.com>
2022-08-26 11:57:25 +0200

    add /search base url for locationSearch route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const authRoutes = require('./auth.routes');
+const locationSearchRoutes = require('./locationSearch.routes');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
@@ -7,5 +8,6 @@ router.get('/', (req, res, next) => {
 });
 
 router.use('/auth', authRoutes);
+router.use('/search', locationSearchRoutes);
 
 module.exports = router;
