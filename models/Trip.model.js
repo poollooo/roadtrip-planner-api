2dbf964f2f8af51a52715edd07adb0e7528601ba
Sebastien Niang <sebastien.niang@hotmail.fr>
2022-08-26 10:01:11 +0200

    updated require field

--- models/Trip.model.js
+++ models/Trip.model.js
@@ -2,11 +2,11 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const tripSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: "User" },
+  userId: { type: Schema.Types.ObjectId, ref: "User", require: true },
   name: Schema.Types.String,
-  locationId: Schema.Types.Number,
-  startDate: Schema.Types.Date,
-  endDate: Schema.Types.Date,
+  locationId: { type: Schema.Types.Number, require: true },
+  startDate: { type: Schema.Types.Date, require: true },
+  endDate: { type: Schema.Types.Date, require: true },
 });
 
 const Trip = model("Trip", tripSchema);
