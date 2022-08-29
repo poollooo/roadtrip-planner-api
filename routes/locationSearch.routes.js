af9e0402ce7d6e412dfdee41e7bc9e3fbbd02549
Paul Nizet <polonizet@gmail.com>
2022-08-29 11:45:21 +0200

    format restaurant data response

--- routes/locationSearch.routes.js
+++ routes/locationSearch.routes.js
@@ -41,24 +41,44 @@ router.get('/:citySearched', getLocationId, async (req, res, next) => {
     optionsRestaurant.params.location_id = req.locationSearchedId;
     optionsActivities.params.location_id = req.locationSearchedId;
     let restaurantList;
-    let attractionList;
+    // let attractionList;
 
     await axios.request(optionsRestaurant)
       .then((response) => {
-        console.log(response.data.data);
         restaurantList = response.data.data;
+        // console.log(restaurantList);
+        restaurantList = restaurantList.map((restaurant) => ({
+          location_id: restaurant.location_id,
+          name: restaurant.name,
+          description: restaurant.description,
+          numberOfReviews: restaurant.num_reviews,
+          photo: restaurant.photo,
+          rawRating: restaurant.raw_ranking,
+          ranking: restaurant.ranking,
+          priceLevel: restaurant.price_level,
+          priceRange: restaurant.price,
+          tripAdvisorUrl: restaurant.web_url,
+          category: restaurant.category,
+          phone: restaurant.phone,
+          website: restaurant.website,
+          email: restaurant.email,
+          address: restaurant.address,
+          hours: restaurant.hours,
+        }));
+        console.log(restaurantList);
       }).catch((error) => {
         console.error(error);
       });
 
-    await axios.request(optionsActivities)
-      .then((response) => {
-        console.log(response.data.data);
-        attractionList = response.data.data;
-      }).catch((error) => {
-        console.error(error);
-      });
-    res.status(200).json({ restaurantList, attractionList });
+    // await axios.request(optionsActivities)
+    //   .then((response) => {
+    //     console.log(response.data.data);
+    //     attractionList = response.data.data;
+    //   }).catch((error) => {
+    //     console.error(error);
+    //   });
+    res.status(200).json({ restaurantList });
+    // res.status(200).json({ restaurantList, attractionList });
   } catch (error) {
     next(error);
   }
