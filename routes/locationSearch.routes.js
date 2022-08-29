f5900dbe3cf45a5146e8f2b3bbce53384655b90f
Paul Nizet <polonizet@gmail.com>
2022-08-29 16:12:56 +0200

    add create Activities and fix external API structure

--- routes/locationSearch.routes.js
+++ routes/locationSearch.routes.js
@@ -1,6 +1,8 @@
+/* eslint-disable max-len */
 const router = require('express').Router();
 const axios = require('axios');
 const { getLocationId } = require('../middleware/getLocationId');
+const ActivitiesTemp = require('../models/ActivitiesTemp.model');
 
 const optionsRestaurant = {
   method: 'GET',
@@ -26,7 +28,7 @@ const optionsActivities = {
     currency: 'USD',
     lang: 'en_US',
     lunit: 'km',
-    limit: '5',
+    limit: '30',
     sort: 'recommended',
   },
   headers: {
@@ -38,7 +40,13 @@ const optionsActivities = {
 // the route is {baseUrl}/api/search/:citySearched
 
 function getHours(hoursArray) {
-  return hoursArray.map((hour) => [hour[0].open_time / 60, hour[0].close_time / 60]);
+  // if the hours array is empty, it means the restaurant is closed
+  if (!hoursArray) {
+    const emptyArray = [0, 0];
+    return emptyArray;
+  }
+  const newHours = hoursArray.map((hour) => [hour[0]?.open_time || 0, hour[0]?.close_time || 0]);
+  return newHours;
 }
 
 router.get('/:citySearched', getLocationId, async (req, res, next) => {
@@ -47,58 +55,83 @@ router.get('/:citySearched', getLocationId, async (req, res, next) => {
     optionsActivities.params.location_id = req.locationSearchedId;
     let restaurantList;
     let activityList;
+    await ActivitiesTemp.deleteMany();
 
     await axios.request(optionsRestaurant)
       .then((response) => {
         restaurantList = response.data.data;
 
-        restaurantList = restaurantList.map((restaurant) => ({
-          location_id: restaurant.location_id,
-          name: restaurant.name,
-          description: restaurant.description,
-          numberOfReviews: restaurant.num_reviews,
-          photo: restaurant.photo?.images?.large?.url,
-          rawRating: restaurant.raw_ranking,
-          ranking: restaurant.ranking,
-          priceLevel: restaurant.price_level,
-          priceRange: restaurant.price,
-          tripAdvisorUrl: restaurant.web_url,
-          category: restaurant.category,
-          phone: restaurant.phone,
-          website: restaurant.website,
-          email: restaurant.email,
-          address: restaurant.address,
-          hours: getHours(restaurant.hours.week_ranges),
-        }));
-      }).catch((error) => {
+        restaurantList = restaurantList.map((restaurant) => {
+          const newRestaurant = {
+            locationId: restaurant.location_id,
+            name: restaurant.name,
+            description: restaurant.description,
+            numberOfReviews: restaurant.num_reviews,
+            photo: restaurant.photo?.images?.large?.url,
+            rawRating: restaurant.raw_ranking,
+            ranking: restaurant.ranking,
+            priceLevel: restaurant.price_level,
+            priceRange: restaurant.price,
+            tripAdvisorUrl: restaurant.web_url,
+            category: restaurant.category?.key,
+            phone: restaurant.phone,
+            website: restaurant.website,
+            email: restaurant.email,
+            address: restaurant.address,
+            hours: getHours(restaurant.hours?.week_ranges),
+          };
+          //   const restaurantToCheck = ActivitiesTemp.find({ locationId: newRestaurant.locationId });
+          //   console.log(restaurantToCheck);
+          //   if (!restaurantToCheck) {
+          if (newRestaurant.name) {
+            console.log('inside !restauToCheck');
+            return ActivitiesTemp.create(newRestaurant);
+          }
+        //   }
+        });
+        Promise.all(restaurantList);
+      })
+      .catch((error) => {
         console.error(error);
       });
 
     await axios.request(optionsActivities)
       .then((response) => {
         activityList = response.data.data;
-        activityList = activityList.map((activity) => ({
-          location_id: activity.location_id,
-          name: activity.name,
-          description: activity.description,
-          numberOfReviews: activity.num_reviews,
-          photo: activity.photo?.images?.large?.url,
-          rawRating: activity.raw_ranking,
-          ranking: activity.ranking,
-          priceLevel: activity.price_level,
-          priceRange: activity.price,
-          tripAdvisorUrl: activity.web_url,
-          category: activity.category,
-          phone: activity.phone,
-          website: activity.website,
-          email: activity.email,
-          address: activity.address,
-          hours: activity.hours,
-        }));
-      }).catch((error) => {
+        activityList = activityList.map((activity) => {
+          const newActivity = {
+            locationId: activity.location_id,
+            name: activity.name,
+            description: activity.description,
+            numberOfReviews: activity.num_reviews,
+            photo: activity.photo?.images?.large?.url,
+            rawRating: activity.raw_ranking,
+            ranking: activity.ranking,
+            priceLevel: activity.price_level,
+            priceRange: activity.price,
+            tripAdvisorUrl: activity.web_url,
+            category: activity.category?.key,
+            phone: activity.phone,
+            website: activity.website,
+            email: activity.email,
+            address: activity.address,
+            hours: getHours(activity.hours?.week_ranges),
+          };
+          //   const activityToCheck = ActivitiesTemp.findOne({ locationId: newActivity.locationId });
+          //   console.log(activityToCheck.locationId);
+          //   if (!activityToCheck) {
+          if (newActivity.name) {
+            return ActivitiesTemp.create(newActivity);
+          }
+        //   }
+        });
+        Promise.all(activityList);
+      })
+      .catch((error) => {
         console.error(error);
       });
-    console.log(restaurantList, activityList);
+    // console.log(restaurantList, activityList);
+    // ActivitiesTemp.create(restaurantList);
     res.status(200).json({ restaurantList, activityList });
   } catch (error) {
     next(error);
