15c95e7adaf984236092e01b5438aca8220e2a67
Paul Nizet <polonizet@gmail.com>
2022-08-29 11:08:38 +0200

    fix eslint and remove useless findOne import (suggested by Yuan)

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,62 +1,61 @@
-const router = require("express").Router();
-const { findOne } = require("../models/User.model");
-const User = require("../models/User.model");
+const router = require('express').Router();
+const User = require('../models/User.model');
 
 /* GET home page */
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const findAll = await User.find();
   res.json({ findAll });
 });
 
-router.patch("/:username", async (req, res, next) => {
+router.patch('/:username', async (req, res, next) => {
   const updatedValue = {};
   if (!req.body.username && !req.body.email) {
-    res.status(400).json({ message: "Nothing to change" });
+    res.status(400).json({ message: 'Nothing to change' });
     return;
   }
-  //getting the user using the params
+  // getting the user using the params
   const userToUpdate = await User.findOne(req.params);
   if (!userToUpdate) {
-    res.status(400).json({ message: "User not found" });
+    res.status(400).json({ message: 'User not found' });
     return;
   }
 
-  //if the username is filled, checking that it is correct
+  // if the username is filled, checking that it is correct
   if (req.body.username) {
-    //checking if the user name is different than the actual one
+    // checking if the user name is different than the actual one
     if (req.body.username === res.params.username) {
       return res
         .status(400)
-        .json({ errorMessage: "This is already your username." });
+        .json({ errorMessage: 'This is already your username.' });
     }
     const existingUser = await User.findOne({ username: req.body.username });
     // If the user is found, send the message username is taken
     if (existingUser) {
-      return res.status(400).json({ errorMessage: "Username already taken." });
+      return res.status(400).json({ errorMessage: 'Username already taken.' });
     }
 
     updatedValue.username = req.body.username;
   }
-  //To be added
+  // To be added
   // if(req.body.email){
 
   // }
   if (req.body.email) {
-    //checking if the user name is different than the actual one
+    // checking if the user name is different than the actual one
     if (req.body.email === userToUpdate.email) {
       return res
         .status(400)
-        .json({ errorMessage: "This is already your email." });
+        .json({ errorMessage: 'This is already your email.' });
     }
     const existingUser2 = await User.findOne({ email: req.body.email });
     // If the user is found, send the message username is taken
     if (existingUser2) {
-      return res.status(400).json({ errorMessage: "Email already taken." });
+      return res.status(400).json({ errorMessage: 'Email already taken.' });
     }
     if (req.body.email === userToUpdate.email) {
       return res
         .status(400)
-        .json({ errorMessage: "This is already your email." });
+        .json({ errorMessage: 'This is already your email.' });
     }
     updatedValue.email = req.body.email;
   }
@@ -66,13 +65,13 @@ router.patch("/:username", async (req, res, next) => {
     updatedValue,
     {
       new: true,
-    }
+    },
   );
 
-  res.status(200).json({ message: "user updated", User: updatedUser });
+  res.status(200).json({ message: 'user updated', User: updatedUser });
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete('/:id', async (req, res) => {
   await User.findByIdAndDelete(req.params.id);
   res.sendStatus(204);
 });
