8eb17a4f2d4b7b86a9bf5f2e65a0e7174992cb0e
Paul Nizet <polonizet@gmail.com>
2022-08-30 10:35:53 +0200

    add Activities route to index.route.js

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,15 +1,17 @@
-const userRoutes = require("./user.routes");
-const authRoutes = require("./auth.routes");
-const router = require("express").Router();
+const userRoutes = require('./user.routes');
+const authRoutes = require('./auth.routes');
+const router = require('express').Router();
+const activitiesRoutes = require('./activities.routes');
 const locationSearchRoutes = require('./locationSearch.routes');
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
+router.get('/', (req, res, next) => {
+  res.json('All good in here');
 });
 
 router.use('/auth', authRoutes);
 router.use('/search', locationSearchRoutes);
-router.use("/users", userRoutes);
+router.use('/users', userRoutes);
+router.use('/activities', activitiesRoutes);
 
 module.exports = router;
