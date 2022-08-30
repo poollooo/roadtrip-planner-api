ad52ce73d87419277b7fb42df8b992f83355c9ca
Paul Nizet <polonizet@gmail.com>
2022-08-30 12:57:16 +0200

    fix server error connecting to mongoose

--- utils/consts.js
+++ utils/consts.js
@@ -1,3 +1,3 @@
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/roadtrip-planner";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/roadtrip-planner"
 
 module.exports = MONGO_URI;
