3187bb3adbddac28dd50ec74b01305dc9f64f9f5
Sebastien Niang <sebastien.niang@hotmail.fr>
2022-08-31 12:53:31 +0200

    reviewed 3

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -4,7 +4,6 @@ module.exports.isAdmin = async (req, res, next) => {
   }
 
   if (req.user.role !== "admin") {
-    console.log("You are not admin");
     return res.status(400).json({ message: "You can't access this route" });
   }
 
