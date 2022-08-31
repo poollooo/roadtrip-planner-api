82c4509bec61f084234d09c5e53c2b68b0ddeec7
Sebastien Niang <sebastien.niang@hotmail.fr>
2022-08-31 12:52:34 +0200

    reviewed 2

--- middleware/isValid.js
+++ middleware/isValid.js
@@ -1,12 +1,12 @@
 module.exports.isValid = async (req, res, next) => {
   if (!req.user) {
-    return res.status(400).json({ message: "No user found!" });
+    return res.status(404).json({ message: "No user found!" });
   }
 
   if (!req.user.isValid) {
     return res
       .status(400)
-      .json({ message: "You need to validate your email to continue" });
+      .json({ message: "You need to verify your email to continue" });
   }
 
   next();
