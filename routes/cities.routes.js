7a75ffff758189c68680ae1d009c491563e5c8ba
Paul Nizet <polonizet@gmail.com>
2022-09-15 15:07:53 +0200

    Add try catch on Cities route

--- routes/cities.routes.js
+++ routes/cities.routes.js
@@ -2,13 +2,21 @@ const router = require('express').Router();
 const City = require('../models/City.model');
 
 router.get('/', async (req, res) => {
-    const findAllActivities = await City.find();
-    res.json({ findAllActivities });
+    try {
+        const findAllActivities = await City.find();
+        res.json({ findAllActivities });
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.get('/:id', async (req, res) => {
-    const cityById = await City.findById(req.params.id)
-    res.json({ cityById })
+    try {
+        const cityById = await City.findById(req.params.id)
+        res.json({ cityById })
+    } catch (error) {
+        next(error);
+    }
 })
 
 
