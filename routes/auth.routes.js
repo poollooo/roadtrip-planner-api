639482e8539bd8f3901386e04e4891262a1bfd54
Paul Nizet <polonizet@gmail.com>
2022-09-19 12:39:20 +0200

    add verify route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -178,6 +178,17 @@ router.post("/login", (req, res, next) => {
     });
 });
 
+router.get('/verify', isAuthenticated, (req, res, next) => {
+
+  // If JWT token is valid the payload gets decoded by the
+  // isAuthenticated middleware and made available on `req.payload`
+  console.log(`req.payload`, req.payload);
+
+  // Send back the object with user data
+  // previously set as the token payload
+  res.status(200).json(req.payload);
+});
+
 //verification of the email (sending)
 
 module.exports = router;
