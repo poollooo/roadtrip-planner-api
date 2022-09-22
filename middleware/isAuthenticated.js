8bfe3554f65b377c642d5b95b368e4233078ac7e
Paul Nizet <polonizet@gmail.com>
2022-09-22 18:25:00 +0200

    add isAuthenticated comments

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -2,14 +2,17 @@ const jsonWebToken = require("jsonwebtoken");
 const User = require("../models/User.model");
 
 const isAuthenticated = async (req, res, next) => {
+  // getting token from header 
   let token = req.headers.authorization;
   if (!token) {
     return res.status(400).json({ message: "No token found!" });
   }
+  // decompose the token and extract the user id by decrypting it
   token = token.replace("Bearer ", "");
   const userToken = jsonWebToken.verify(token, process.env.TOKEN_SECRET);
 
   try {
+    // search for the user with the encrypted id in the database
     const user = await User.findById(userToken.user).select("-password");
 
     if (!user) {
